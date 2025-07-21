c3b05c08d7d19719ea3437518d5e22450372c85f
Zulfiqor <siapaa249@gmail.com>
2025-07-21 21:19:01 +0700

    feat : add obat-masuk add

---
+++ frontend/src/app/obat-masuk/add/page.tsx
@@ -0,0 +1,241 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import DashboardLayout from "../../dashboard/layout";
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { TextField, Button, IconButton, Box, InputLabel, MenuItem, FormControl, CircularProgress } from '@mui/material';
+import Select from '@mui/material/Select';
+import {fetchObat, fetchObatMasukAdd, fetchSupplier } from '@/services';
+import useStore from '@/store/useStore';
+import { ArrowBack } from '@mui/icons-material';
+import { toast } from 'react-hot-toast';
+import dayjs, { Dayjs } from 'dayjs';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+interface FormData {
+    obat: string;
+    tanggal: Dayjs | null;
+    user: string;
+    supplier: string;
+}
+
+interface dataResponse {
+    id: number;
+    nama_obat: string;
+    user: {
+        id: number;
+        name: string;
+    }
+}
+
+interface dataResponseSupplier {
+    id: number;
+    nama: string;
+    alamat: string;
+    email: string;
+    no_telepon: string;
+}
+
+const Page: React.FC = () => {
+    const { register, handleSubmit, reset, formState: { errors }, control, setValue } = useForm<FormData>();
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState<dataResponse[]>([]);
+    const [dataSupplier, setDataSupplier] = useState<dataResponseSupplier[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [tanggal, setTanggal] = useState<Dayjs | null>(dayjs());
+
+    const router = useRouter();
+    const { user } = useStore();
+
+    const fetchData = async () => {
+        if (!user || !user.token) return;
+
+        try {
+            const response = await fetchObat(user?.token);
+            const result = response.data;
+
+            setData(result);
+            setLoading(false);
+
+        } catch (error: any) {
+            toast.error(error.response?.data?.message || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchDataSupplier = async () => {
+        if (!user || !user.token) return;
+
+        try {
+            const response = await fetchSupplier(user?.token);
+            const result = response.data;
+
+            setDataSupplier(result);
+            setLoading(false);
+
+        } catch (error: any) {
+            toast.error(error.response?.data?.message || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+        fetchDataSupplier();
+    }, [user]);
+
+
+    const onSubmit: SubmitHandler<FormData> = async (data) => {
+        if (!user || !user.token) return;
+        setLoading(true);
+        try {
+            const postData = {
+                nama_obat: data.obat,
+                id_obat: data.obat,
+                tanggal_masuk: tanggal?.format('YYYY-MM-DD'),
+                id_supplier: data.supplier,
+                id_user: data.user,
+            };            
+            const response = await fetchObatMasukAdd(user?.token, postData);
+            toast.success(response.message || "Data berhasil Ditambahkan!");
+            reset();
+            setTanggal(dayjs());
+            router.back();
+        } catch (error: any) {
+            toast.error(error.response?.data?.message || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <DashboardLayout>
+            <div className="flex flex-row mt-4 ml-4 mr-4 mb-10">
+                <IconButton onClick={() => router.back()}>
+                    <ArrowBack />
+                </IconButton>
+                <h1 className="text-2xl font-semibold mt-1 ml-2">Tambah Data Obat</h1>
+                {
+                    loading && <CircularProgress />
+                }
+            </div>
+
+            <div className='ml-7'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-5 w-full">
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DatePicker
+                                label="Tanggal Masuk"
+                                value={tanggal}
+                                onChange={(newValue) => {
+                                    setTanggal(newValue);
+                                    setValue('tanggal', newValue, { shouldValidate: true });
+                                }}
+                            />
+                        </LocalizationProvider>
+                    </div>
+
+                    <div className="w-1/3 mr-5 mb-5">
+                        <FormControl fullWidth error={!!errors.obat}>
+                            <InputLabel id="obat-label">Nama Obat</InputLabel>
+                            <Controller
+                                name="obat"
+                                control={control}
+                                defaultValue=""
+                                rules={{ required: "Nama Obat is required" }}
+                                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                    <Select
+                                        labelId="obat-label"
+                                        id="obat"
+                                        value={value}
+                                        label="Obat"
+                                        onChange={onChange}
+                                    >
+                                        {data.map((item) => (
+                                            <MenuItem key={item.id} value={item.id}>{item?.nama_obat}</MenuItem>
+                                        ))}
+                                    </Select>
+                                )}
+                            />
+                            {errors.obat && <p className="MuiFormHelperText-root Mui-error MuiFormHelperText-sizeMedium MuiFormHelperText-contained mui-1wc848c-MuiFormHelperText-root" id="obat">{errors.obat.message}</p>}
+                        </FormControl>
+                    </div>
+
+                    <div className="w-1/3 mr-5 mb-5">
+                        <FormControl fullWidth error={!!errors.obat}>
+                            <InputLabel id="obat-label">Penerima</InputLabel>
+                            <Controller
+                                name="user"
+                                control={control}
+                                defaultValue=""
+                                rules={{ required: "Penerima is required" }}
+                                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                    <Select
+                                        labelId="user-label"
+                                        id="user"
+                                        value={value}
+                                        label="Penerima"
+                                        onChange={onChange}
+                                    >
+                                        {data.map((item) => (
+                                            <MenuItem key={item.id} value={item.id}>{item?.user.name}</MenuItem>
+                                        ))}
+                                    </Select>
+                                )}
+                            />
+                            {errors.user && <p className="MuiFormHelperText-root Mui-error MuiFormHelperText-sizeMedium MuiFormHelperText-contained mui-1wc848c-MuiFormHelperText-root" id="user">{errors.user.message}</p>}
+                        </FormControl>
+                    </div>
+
+                    <div className="w-1/3 mr-5 mb-5">
+                        <FormControl fullWidth error={!!errors.obat}>
+                            <InputLabel id="obat-label">Supplier</InputLabel>
+                            <Controller
+                                name="supplier"
+                                control={control}
+                                defaultValue=""
+                                rules={{ required: "Supplier is required" }}
+                                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                    <Select
+                                        labelId="supplier-label"
+                                        id="supplier"
+                                        value={value}
+                                        label="Supplier"
+                                        onChange={onChange}
+                                    >
+                                        {dataSupplier.map((item) => (
+                                            <MenuItem key={item.id} value={item.id}>{item?.nama}</MenuItem>
+                                        ))}
+                                    </Select>
+                                )}
+                            />
+                            {errors.supplier && <p className="MuiFormHelperText-root Mui-error MuiFormHelperText-sizeMedium MuiFormHelperText-contained mui-1wc848c-MuiFormHelperText-root" id="supplier">{errors.supplier.message}</p>}
+                        </FormControl>
+                    </div>
+
+                    <div className="mt-8">
+                        <Button
+                            size='large'
+                            className='w-1/6'
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            disabled={loading}
+                            style={{ textTransform: 'none' }}
+                        >
+                            {loading ? 'Loading...' : 'Tambah'}
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </DashboardLayout>
+    );
+};
+
+export default Page;
