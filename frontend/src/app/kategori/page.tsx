abc67d516020833f8ccad4abdd0eac433aae3f8e
Zulfiqor <siapaa249@gmail.com>
2025-07-22 20:49:29 +0700

    ✨ modify

--- frontend/src/app/kategori/page.tsx
+++ frontend/src/app/kategori/page.tsx
@@ -5,11 +5,12 @@ import { useRouter } from 'next/navigation';
 import DashboardLayout from "../dashboard/layout";
 import Link from 'next/link';
 import { Add, Delete, Edit } from "@mui/icons-material";
-import { CustomButton, ButtonCustom } from "@/components";
+import { CustomButton, ButtonCustom, CustomModal } from "@/components";
 import { fetchKategori, fetchKategoriDelete } from '@/services';
 import useStore, { User } from '@/store/useStore'
 import { Table, TablePagination, TableHead, TableRow, TableCell, TableBody, CircularProgress, Button, Alert, AlertTitle, TextField } from '@mui/material';
 import { toast } from 'react-hot-toast';
+import { set } from 'react-hook-form';
 
 interface dataResponse {
     message: string
@@ -21,6 +22,8 @@ const Page: React.FC = () => {
     const [data, setData] = useState<any[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState<boolean>(false);
+    const [itemId, setItemId] = useState<string>('');
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
     const [searchQuery, setSearchQuery] = useState<string>('');
@@ -31,9 +34,9 @@ const Page: React.FC = () => {
 
         try {
             const apiData = await fetchKategori(user?.token);
-            setData(apiData.data);  
+            setData(apiData.data);
             setLoading(false);
-            
+
         } catch (error: any) {
             toast.error(error.response?.data?.message || error.message);
         } finally {
@@ -66,10 +69,12 @@ const Page: React.FC = () => {
             toast.success(apiData.message || "Data berhasil Dihapus!");
             fetchData();
             setLoading(false);
+            setOpen(false);
         } catch (error: any) {
             toast.error(error.response?.data?.message || error.message);
         } finally {
             setLoading(false);
+            setOpen(false)
         }
     };
 
@@ -80,94 +85,100 @@ const Page: React.FC = () => {
 
     const filteredData = data.filter(item =>
         item.nama.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        item.penerbit.toLowerCase().includes(searchQuery.toLowerCase()) 
+        item.penerbit.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
     return (
-        <DashboardLayout>
-            {
-                loading && <CircularProgress />
-            }
+        <React.Fragment>
+            <DashboardLayout>
+                {
+                    loading && <CircularProgress />
+                }
 
-            <div className="flex mt-4 mr-5 ml-5 mb-5 justify-between">
-                <h1 className="text-2xl font-semibold">Data Kategori</h1>
+                <div className="flex mt-4 mr-5 ml-5 mb-5 justify-between">
+                    <h1 className="text-2xl font-semibold">Data Kategori</h1>
 
-                <div className="">
-                    <Link href={'/kategori/add'}>
-                        <CustomButton startIcon={<Add />}>
-                            Tambah
-                        </CustomButton>
-                    </Link>
+                    <div className="">
+                        <Link href={'/kategori/add'}>
+                            <CustomButton startIcon={<Add />}>
+                                Tambah
+                            </CustomButton>
+                        </Link>
+                    </div>
                 </div>
-            </div>
 
-            <div className="ml-5 mb-6 w-1/3">
-                <TextField
-                    label="Cari Kategori"
-                    variant="outlined"
-                    fullWidth
-                    value={searchQuery}
-                    onChange={handleSearchChange}
-                />
-            </div>
+                <div className="ml-5 mb-6 w-1/3">
+                    <TextField
+                        label="Cari Kategori"
+                        variant="outlined"
+                        fullWidth
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                    />
+                </div>
 
-            <div className='mx-5'>
-                <Table>
-                    <TableHead className='bg-green-700'>
-                        <TableRow>
-                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>No</TableCell>
-                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Nama Kategori</TableCell>
-                            <TableCell sx={{ color: 'white', fontWeight: '600' }}>Penerbit</TableCell>
-                            <TableCell sx={{ color: 'white', fontWeight: '600', textAlign: 'center' }}>Action</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
-                            <TableRow key={item.id}>
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell>{item.nama}</TableCell>
-                                <TableCell>{item.penerbit}</TableCell>
-                                <TableCell>
-                                    <div className='flex flex-row justify-center'>
-                                        <div className='mr-2'>
-                                            <ButtonCustom
-                                                color='success'
-                                                onClick={() => handleEdit(item.id)}
-                                                fontSize="0.75rem"
-                                                textTransform="none"
-                                                variant='outlined'
-                                            >
-                                                Edit
-                                            </ButtonCustom>
-                                        </div>
-                                        <div>
-                                            <ButtonCustom
-                                                color="error"
-                                                onClick={() => handleDelete(item.id)}
-                                                variant="outlined"
-                                                textTransform='none'
-                                                fontSize="0.75rem"
-                                            >
-                                                Delete
-                                            </ButtonCustom>
-                                        </div>
-                                    </div>
-                                </TableCell>
+                <div className='mx-5'>
+                    <Table>
+                        <TableHead className='bg-green-700'>
+                            <TableRow>
+                                <TableCell sx={{ color: 'white', fontWeight: '600' }}>No</TableCell>
+                                <TableCell sx={{ color: 'white', fontWeight: '600' }}>Nama Kategori</TableCell>
+                                <TableCell sx={{ color: 'white', fontWeight: '600' }}>Penerbit</TableCell>
+                                <TableCell sx={{ color: 'white', fontWeight: '600', textAlign: 'center' }}>Action</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={data.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </div>
-        </DashboardLayout>
+                        </TableHead>
+                        <TableBody>
+                            {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
+                                <TableRow key={item.id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{item.nama}</TableCell>
+                                    <TableCell>{item.penerbit}</TableCell>
+                                    <TableCell>
+                                        <div className='flex flex-row justify-center'>
+                                            <div className='mr-2'>
+                                                <ButtonCustom
+                                                    color='success'
+                                                    onClick={() => handleEdit(item.id)}
+                                                    fontSize="0.75rem"
+                                                    textTransform="none"
+                                                    variant='outlined'
+                                                >
+                                                    Edit
+                                                </ButtonCustom>
+                                            </div>
+                                            <div>
+                                                <ButtonCustom
+                                                    color="error"
+                                                    onClick={() => {
+                                                        setItemId(item.id);
+                                                        setOpen(true)
+                                                    }}
+                                                    variant="outlined"
+                                                    textTransform='none'
+                                                    fontSize="0.75rem"
+                                                >
+                                                    Delete
+                                                </ButtonCustom>
+                                            </div>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={data.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </div>
+            </DashboardLayout>
+            <CustomModal open={open} onClose={() => setOpen(false)} onConfirm={() => handleDelete(itemId)} />
+        </React.Fragment>
     )
 }
 
