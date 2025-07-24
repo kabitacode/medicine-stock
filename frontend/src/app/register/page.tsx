52b139658a01e06592416d10170f225be9358d5c
Zulfiqor <siapaa249@gmail.com>
2025-07-24 13:57:55 +0700

    ✨

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -65,7 +65,7 @@ const Page: React.FC = () => {
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
               type="text"
-              placeholder="Zhoel"
+              placeholder="Nama"
               {...register('name', { required: true })}
             />
             {errors.email && <p className="mt-3 text-sm text-red-500">Nama is required</p>}
@@ -106,7 +106,7 @@ const Page: React.FC = () => {
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
               type="email"
-              placeholder="john@example.com"
+              placeholder="name@mail.com"
               {...register('email', { required: true })}
             />
             {errors.email && <p className="mt-3 text-sm text-red-500">Email is required</p>}
